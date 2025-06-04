f8ecbaa6ae7a85a68f4a72d29f2193a13776aa12
joaoP2006 <joao.pv09@aluno.ifsc.edu.br>
2025-06-04 12:07:21 -0300

    phpEX

---
+++ php/1.php
@@ -0,0 +1,15 @@
+
+<?php
+ echo "Olá Mundao";
+ $nome = "Joao";
+
+ echo gettype($nome). "<br>";
+ echo $nome;
+
+ $curso = $_GET["nome"];
+ echo $curso;
+
+ var_dump($_SERVER)
+
+?>
+    
---
+++ php/4.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h2>Imprima todos os termos de uma P. A N informado pelo usuario</h2>
+
+<label for="termon">Informe o indice do termo</label>
+<input id = "termon" type="text" name="n">
+a1= 0 
+r=3
+
+<?php 
+$a1=0;
+$r=3;
+
+
+$n isset($_GET["n"])?$_GET["n"]:1;
+$n isset($_GET["n"])?$_GET["n"]:1;
+$i = 1;
+$termon=
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+    
+</body>
+</html>

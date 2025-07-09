2149207d86c471b2782b07b6c011bee630eeb3f9
joaoP2006 <joao.pv09@aluno.ifsc.edu.br>
2025-07-09 10:19:04 -0300

    Pronto? talvez

--- crud/controlador/AuthController.php
+++ crud/controlador/AuthController.php
@@ -1,27 +1,32 @@
 <?php
-include_once __DIR__. './../modelo/Conexao.php';
-include_once __DIR__. './../modelo/Usuario.php';
-include_once __DIR__. './../modelo/UsuarioDAO.php';
-class AuthController{
+
+include_once __DIR__.'./../modelo/Conexao.php';
+include_once __DIR__.'./../modelo/Usuario.php';
+include_once __DIR__.'./../modelo/UsuarioDAO.php';
+
+ class AuthController{
 
     public function login(){
-        $dao= new UsuarioDAO();
 
-        if(isset($_POST['login']) && isset($_POST['senha'])){
-            $login=$_POST['login'];
-            $senha=$_POST['senha'];
-            $dao->autenticar($login,$senha)
-            if(!isset($usr)){
-                header("Location:rota.php?rota=home");
-            }
-        }else{
+    $dao = new UsuarioDAO();
+
+    if(isset($_POST['email']) && isset($_POST['senha'])){
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        $usuario = $dao->autenticar($email,$senha);
+   
+        if(isset($usuario)){
+            $_SESSION['usuario']=$usuario->id;
+            header("Location:rota.php?rota=home");
+        }
+        else{
             header("Location:rota.php");
-
-        }else{
-            header("location:rota.php");
+        }
+    }
+        else{
+            header("Location:rota.php");
         }
     }
 }
-
 
 ?>

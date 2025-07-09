2728b4c7467a86ecd00072e1c0ea9553ba6ebdf1
joaoP2006 <joao.pv09@aluno.ifsc.edu.br>
2025-07-09 09:39:29 -0300

    Crud modificaçoes

---
+++ crud/rota.php
@@ -0,0 +1,53 @@
+<?php
+
+include_once __DIR__.'./Modelo/Conexao.php';
+include_once __DIR__.'./Modelo/Usuario.php';
+include_once __DIR__.'./Modelo/UsuarioDAO.php';
+include_once __DIR__.'./Modelo/UsuarioDAO.php';
+include_once __DIR__.'./Controlador/AuthController.php';
+include_once __DIR__.'./Controlador/CadastroController.php';
+session_start();
+
+
+if(isset($_GET['rota'])){
+    $rota = $_GET['rota'];
+}else{
+    $rota="login";
+}
+
+switch($rota){
+    case 'login':
+
+        if(isset($_SESSION['usuario'])){
+            header("Location:home.php");
+        }
+        require "login.php";
+        break;
+
+    case 'autenticacao':
+        $aut = new AuthController();
+        $aut->login();
+        break;
+    
+    case 'cadastro':
+        $cadastro = new CadastroController();
+        $cadastro ->cadastro();
+            header("Location:CadastroUsuario.php");
+            break;
+
+    case 'home':
+        header("Location:home.php");
+        break;
+
+    case 'logout':
+            session_destroy();
+
+            header("Location:rota.php");
+            break;
+
+
+    default:
+    echo "rota desconhecida";
+}
+
+?>
